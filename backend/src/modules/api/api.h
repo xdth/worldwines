461105c5a2c641d39e0cc3f9aff8d8bdff4c2f53
dth <dth@overkill.labs.dth>
2023-06-09 13:18:09 -0300

    refactoring and linting

--- backend/src/modules/api/api.h
+++ backend/src/modules/api/api.h
@@ -5,19 +5,21 @@
 // Includes
 
 #include <cpprest/http_listener.h>
+#include "../database/database.h"
 
 
 //--------------------------------------------------------------------
 // Namespaces
 
-// using namespace web;
+using namespace web;
 using namespace web::http;
-// using namespace web::http::experimental::listener;
+using namespace web::http::experimental::listener;
 
 
 //--------------------------------------------------------------------
 // Function prototypes
 
+json::value api_return_json(Wine wine);
 void api_list_by_id(const http_request& request);
 void api_list500(const http_request& request);
 int api_start();
