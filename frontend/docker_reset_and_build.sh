cd7cdc9701ff97c1c994ad519890dde176dd6396
dth <dth@overkill.labs.dth>
2023-06-12 22:18:09 -0300

    adding updating docker config

--- frontend/docker_reset_and_build.sh
+++ frontend/docker_reset_and_build.sh
@@ -4,5 +4,5 @@ docker stop wines_frontend
 docker rm wines_frontend
 docker rmi wines_frontend
 docker build -t wines_frontend .
-# docker run --name wines_frontend -dp 8080:8080 wines_frontend # detached
-docker run -it --name wines_frontend -p 8080:8080 wines_frontend
+# docker run --name wines_frontend -dp 5000:5000 wines_frontend # detached
+docker run -it --name wines_frontend -p 5000:5000 wines_frontend
